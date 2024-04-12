90554e655d86aca006ee3e354c0841212eb74941
David Tanzer <david@davidtanzer.net>
2024-04-12 17:56:28 +0200

    Refactored other items and if blocks to when

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,67 +6,23 @@ class GildedRose(var items: List<Item>) {
         for (i in items.indices) {
 			val item = items[i]
 
-            if (item.name == "Sulfuras, Hand of Ragnaros") {
-                updateSulfuras(item)
-                continue
-            }
-            if (item.name == "Aged Brie") {
-                updateAgedBrie(item)
-                continue
-            }
-            if(item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                updateBackstagePasses(item)
-                continue
-            }
-
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
-            }
-
-            if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality = item.quality - 1
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
-                }
+            when(item.name) {
+                "Sulfuras, Hand of Ragnaros" -> updateSulfuras(item)
+                "Aged Brie" -> updateAgedBrie(item)
+                "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePasses(item)
+                else -> updateOtherItem(item)
             }
         }
+    }
+
+	private fun updateOtherItem(item: Item) {
+        if (item.sellIn <= 0) {
+            item.quality = (item.quality - 2).coerceAtLeast(0)
+        } else {
+            item.quality = (item.quality - 1).coerceAtLeast(0)
+        }
+        
+        item.sellIn = item.sellIn - 1
     }
 
 	private fun updateBackstagePasses(backstagePasses: Item) {
@@ -81,7 +37,6 @@ class GildedRose(var items: List<Item>) {
         }
 
         backstagePasses.sellIn = backstagePasses.sellIn - 1
-
     }
 
 	private fun updateAgedBrie(agedBrie: Item) {
