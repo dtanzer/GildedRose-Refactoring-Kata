90587fd3c51cde0eee8f1ba9e4b8c697714fdd8a
David Tanzer <david@davidtanzer.net>
2024-04-17 08:36:47 +0200

    Re-indented using tabs

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test
 
 internal class GildedRoseTest {
 
-    @Test
-    fun foo() {
-        val items = listOf(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+	@Test
+	fun foo() {
+		val items = listOf(Item("foo", 0, 0))
+		val app = GildedRose(items)
+		app.updateQuality()
+		assertEquals("fixme", app.items[0].name)
 
-    }
+	}
 
 }
 
