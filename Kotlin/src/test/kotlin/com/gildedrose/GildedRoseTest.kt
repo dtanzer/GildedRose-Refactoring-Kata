9d8b40f34bc92e84efbb8f121c4d00ad80dd8272
David Tanzer <david@davidtanzer.net>
2024-04-17 11:09:57 +0200

    First test for sulfuras: Quality

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
 	@Test
-	fun foo() {
-		val items = listOf(Item("foo", 0, 0))
+	fun `quality of sulfuras does not degrade over time`() {
+		val items = listOf(Item("Sulfuras, Hand of Ragnaros", 20, 80))
 		val app = GildedRose(items)
+
 		app.updateQuality()
-		assertEquals("fixme", app.items[0].name)
+
+		assertEquals(80, app.items[0].quality)
 
 	}
 
